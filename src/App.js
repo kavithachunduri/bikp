c810c8c7075328695f4e59e9d67175e6902305d4
poovaic <poovai.c@gmail.com>
2022-08-02 15:59:00 -0400

    poovai-final-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import './App.css';
-import Poovai from '../Components/Poovai'
+
+import {NavLink,Routes,Route} from 'react-router-dom';
+//import Bezawit from './Components/Bezawit'
+//import Kavitha from './Components/Kavitha'
+import Poovai from './Components/Poovai'
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
-      <Poovai/>
+         
+          <nav>
+
+            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/poovai"}>Poovai</NavLink>
+          
+          </nav>
+          
+          
+          <Routes>
+          
+          <Route path="/" element={<Home/>}/>
+          <Route path="/poovai" element={<Poovai/>}/>
+          
+          </Routes>
+    
     </div>
   );
 }
