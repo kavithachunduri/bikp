d932fb45ab2a48547fb6e9f5dc780f34816bb249
bezawitdev <getbezawit@gmail.com>
2022-08-02 16:13:09 -0400

    bezawitChanges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route, Routes, NavLink } from 'react-router-dom';
+import Home from './Component/Home';
+import Bezawit from './Component/Bezawit';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    
+<div className="App">
+     
+       <nav>
+              <NavLink to={"/"} className="nav-links">Home</NavLink>
+             
+             <NavLink to={"/Bezawit"} className="nav-links">Bezawit</NavLink>
+            
+        </nav>
+
+         <Routes>
+
+             <Route path='/' element={<Home />} />
+             <Route path='Bezawit' element={<Bezawit />}/>
+             {/* <Route path='*' element={<NoMatch />}/> */}
+
+         </Routes>
+     </div>
+ )
 }
 
-export default App;
+  
+
+export default App
+  
