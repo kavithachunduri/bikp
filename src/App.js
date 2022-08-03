da3331b441ea875e1544d0b770a7607d464cc10b
kavithachunduri <chunduri.kavitha@gmail.com>
2022-08-03 00:56:00 -0400

    edited app.js

--- src/App.js
+++ src/App.js
@@ -15,15 +15,19 @@ function App() {
 
           <nav>
             <NavLink to={"/"} className="nav-links">Home</NavLink>
-           
+            <NavLink to={"/poovai"} className="nav-links">Poovai</NavLink>
+            <NavLink to={"/bezawit"} className="nav-links">Bezawit</NavLink>
+            <NavLink to={"/imane"} className="nav-links">Imane</NavLink>
             <NavLink to={"/kavitha"} className="nav-links">Kavitha</NavLink>
           </nav>
 
           <Routes>
 
           <Route path="/" element={<Home/>}/>
-        
           <Route path="/kavitha" element={<Kavitha/>}/>
+          <Route path="/poovai" element={<Poovai/>}/>
+          <Route path="/imane" element={<Imane/>}/>
+          <Route path="/bezawit" element={<Bezawit/>}/>
           </Routes>
 
     </div>
