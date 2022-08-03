7bd5fda42f9b5678acdf6e49cb102e3fb58551d3
kavithachunduri <chunduri.kavitha@gmail.com>
2022-08-03 00:16:54 -0400

    added component

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h1>Welcome to BIKP's Group Github Collaboration!!!</h1>
+            <div className='names'>
+            <h3>Bezawit</h3>
+            <h3>Imane</h3>
+            <h3>Kavitha</h3>
+            <h3>Poovai</h3>
+            </div>
+    </div>
+  )
+}
+
+export default Home
